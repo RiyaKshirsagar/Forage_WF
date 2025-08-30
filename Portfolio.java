e31034c1b3cff8eeec55cf18427aeca0439f76a8
RiyaKshirsagar <89297606+RiyaKshirsagar@users.noreply.github.com>
2025-08-30 13:09:00 +0530

    Create Portfolio.java

---
+++ Portfolio.java
@@ -0,0 +1,32 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Security> securities;
+
+    public Portfolio() {}
+
+    public Portfolio(Client client) {
+        this.client = client;
+    }
+
+    // Getters & Setters
+    public Long getPortfolioId() { return portfolioId; }
+    public Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client; }
+    public List<Security> getSecurities() { return securities; }
+    public void setSecurities(List<Security> securities) { this.securities = securities; }
+}
